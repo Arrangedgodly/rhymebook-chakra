a2d197fc3adb8c56706f458f42fcc8715b82ffea
Arrangedgodly <arrangedgodly@gmail.com>
2023-01-29 17:17:11 -0700

    refactor: working on restyling the welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,19 @@
-import { Box, Button } from "@chakra-ui/react";
+import { Heading, Center, Button, Box, useColorModeValue, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 function Welcome({ handleButtonClick }) {
+  const bg = useColorModeValue("gray.400", "gray.800");
+  const box = useColorModeValue('gray.600', 'gray.500');
+  const text = useColorModeValue('gray.900', 'gray.50');
+
   return (
-    <Box className="Welcome">
-      <h1 className="Welcome-header">Welcome to Rhymebook!</h1>
-      <p className="Welcome-text">
+    <Box className="Welcome" bg={bg}>
+      <Center bg={box} rounded='xl' minH='60vh' minW='55vw' flexDirection='column' rowGap='6vh'>
+      <Heading fontSize='6xl' color={text}>Welcome to Rhymebook!</Heading>
+      <Text fontSize='4xl' color={text} maxW='50%'>
         The API powered notebook that writes your rhymes (and much more) with you!
-      </p>
-      <p className="Welcome-subtext">
+      </Text>
+      <Text fontSize='2xl' color={text}>
         Are you a{" "}
         <Link to='/new-user'>
         <Button
@@ -33,7 +38,8 @@ function Welcome({ handleButtonClick }) {
         </Button>
         </Link>
         {" "}?
-      </p>
+      </Text>
+      </Center>
     </Box>
   );
 }
